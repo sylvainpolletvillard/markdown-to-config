d752606f429d84ccd6bdba1c5bfce76fef186a4d
sylvainpolletvillard <sylvain.pollet.villard@gmail.com>
2020-11-25 18:29:55 +0100

    add node 13 support

--- src/fn-utils.js
+++ src/fn-utils.js
@@ -2,4 +2,4 @@ export function compose(f, ...fns) {
 	return (...args) => fns.reduce((x, g) => g(x), f(...args))
 }
 
-export const defaultTo = (defaultVal) => (x) => x ?? defaultVal
+export const defaultTo = (defaultVal) => (x) => (x ? x : defaultVal);
