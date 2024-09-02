d71672ccd86624494d0adaef1f25a48ecb675da5
Sylvain Pollet-Villard <sylvain.pollet.villard@gmail.com>
2024-09-02 18:47:39 +0200

    upgrade marked

--- src/content.js
+++ src/content.js
@@ -1,4 +1,4 @@
-import {marked } from "marked";
+import { marked } from "marked";
 import { parseKey } from "./string-utils.js";
 
 export const HAS_CONTENT = Symbol("HAS_CONTENT");
@@ -6,31 +6,39 @@ export const CONTENT_AS_TEXT = Symbol("CONTENT_AS_TEXT");
 export const CONTENT_AS_HTML = Symbol("CONTENT_AS_HTML");
 
 export function setContent(conf, path, text, options) {
-	const isEmpty = /^[\n\r\s]*$/.test(text)
+	const isEmpty = /^[\n\r\s]*$/.test(text);
 
-	let maxDepth = path.length;
+	const maxDepth = path.length;
 	let depth = 0;
 	let node = conf;
 
 	while (depth < maxDepth) {
-		let key = parseKey(path[depth++], options);
+		const key = parseKey(path[depth++], options);
 		if (!node.hasOwnProperty(key)) {
 			node[key] = {};
 		} else if (typeof node[key] !== "object") {
-			node[key] = { [CONTENT_AS_TEXT]: options.parseContent(node[key]), [HAS_CONTENT]: true };
+			node[key] = {
+				[CONTENT_AS_TEXT]: options.parseContent(node[key]),
+				[HAS_CONTENT]: true,
+			};
 		}
 		node = node[key];
 	}
 
-	if(!isEmpty || node[HAS_CONTENT]) addContent(node, text, options);
+	if (!isEmpty || node[HAS_CONTENT]) addContent(node, text, options);
 }
 
 export function addContent(node, text, options) {
 	if (node.hasOwnProperty(CONTENT_AS_TEXT)) {
-		node[CONTENT_AS_TEXT] = options.parseContent(node[CONTENT_AS_TEXT] + "\n\n" + text);
+		node[CONTENT_AS_TEXT] = options.parseContent(
+			`${node[CONTENT_AS_TEXT]}\n\n${text}`,
+		);
 	} else {
 		node[HAS_CONTENT] = true;
 		node[CONTENT_AS_TEXT] = options.parseContent(text);
 	}
-	node[CONTENT_AS_HTML] = marked.parse(node[CONTENT_AS_TEXT], options.markedOptions);
+	node[CONTENT_AS_HTML] = marked.parse(
+		node[CONTENT_AS_TEXT],
+		options.markedOptions,
+	);
 }
